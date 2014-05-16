4b7b1af651e0a339627cd4f2dfbdb7e673f742bd
Mike Mackintosh <mikemackintosh@users.noreply.github.com>
2014-05-16 15:39:21 -0400

    Update lldp.rb

--- lldp.rb
+++ lldp.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Lldp < Formula
   homepage 'http://www.open-lldp.org/'
   url 'http://sourceforge.net/settings/mirror_choices?projectname=openlldp&filename=openlldp-0.4alpha.zip'
-  sha1 'caa632dc1b302406d42bc1c44cf682587a05e78e'
+  sha1 '353a5843e9bd76c6709fd414293f671e00488f8b'
     
   def install
 
